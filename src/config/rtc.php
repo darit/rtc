78c8854c19524c55b696aa732268b7612a142ba0
Daniel Rodríguez Guerrero <danrodri@mx1.ibm.com>
2016-07-12 16:42:29 -0500

    removed the default config

--- src/config/rtc.php
+++ src/config/rtc.php
@@ -6,8 +6,8 @@
  * @author     Written by Daniel Rodriguez <danrodri@mx1.ibm.com>, November 2015
  */
 return [
-    'host' => 'https://swgjazz.ibm.com:8017/jazz',
-    'namespace' => 'https://swgjazz.ibm.com:8017/jts/users/',
+    'host' => '', //https://domain.tld:port/jazz
+    'namespace' => '', //https://domain.tld:port/jts/users/
     'user' => env('RTC_USERNAME', ''),
     'pass' => env('RTC_PASSWORD', ''),
 ];
